188faebf24289c020d52e3a216c37926ff5326b8
didibao <didibao@163.com>
2019-09-26 18:23:59 +1000

    update

---
+++ Example/Pods/Headers/Public/GitDawg/GDWelcomeViewController.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/GDWelcomeViewController.h
---
+++ Example/Pods/Target Support Files/GitDawg/GitDawg-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GitDawg : NSObject
+@end
+@implementation PodsDummy_GitDawg
+@end
